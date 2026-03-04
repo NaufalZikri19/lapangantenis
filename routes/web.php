fcae67d427aca4efe86b8602bfb1e904f2681796
Naufal Zikri Hilmy <145980027+NaufalZikri19@users.noreply.github.com>
2026-03-04 13:55:25 +0700

    redirect login

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,18 @@ Route::get('/', function () {
 
 Route::view('/contact', 'contact')->name('contact');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard/booking', function () {
-    return view('booking');
-})->name('booking');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'nocache')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::get('/dashboard/booking', function () {
+        return view('booking');
+    })->name('booking');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
